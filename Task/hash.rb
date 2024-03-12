fafb0ab8e2e2d5ad39efd5aa2a5fa02fb3cd01ed
UshaSuwal <usaasuwa@gmail.com>
2024-03-12 12:11:10 +0545

    hash task

---
+++ Task/hash.rb
@@ -0,0 +1,59 @@
+# input "Ram Shyam Hari Ram"
+# output=>
+# Ram:2
+# Shyam:1
+# Hari:1
+
+puts "enter a sentence"
+sentence=gets.chomp
+arr=sentence.downcase.split(" ")
+puts arr
+
+name=Hash.new(0)
+
+arr.each do |i|
+    name[i]+=1
+end
+
+puts name
+
+
+name.each do |nameKey, nameValue| 
+    puts "#{nameKey}:#{nameValue}" 
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# name = Hash.new(0)
+
+
+# arr.each do |i| 
+#     name[i] += 1 
+# end
+
+
+# name.each do |i, count| 
+#     puts "#{i}:#{count}" 
+# end
+
+
+
